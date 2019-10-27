3ee9a80d4564212ac4478070045b3974fed5048d
mrcyjanek <mrcyjanek@gmail.com>
2019-10-27 16:38:57 +0100

    Wallet

---
+++ web/wallet.php
@@ -0,0 +1,50 @@
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: login.php");
+  }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wallet</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="header">
+	<h2><?php echo explode(PHP_EOL,shell_exec("whoami"))[0]; ?>'s wallet!</h2>
+</div>
+<div class="content">
+  	<!-- notification message -->
+  	<?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+      	<h3>
+          <?php 
+          	echo $_SESSION['success']; 
+          	unset($_SESSION['success']);
+          ?>
+      	</h3>
+      </div>
+  	<?php endif ?>
+
+    <!-- logged in user information -->
+    <?php  if (isset($_SESSION['username'])) : ?>
+    	<p>Welcome <strong><?php echo explode(PHP_EOL,shell_exec("whoami"))[0]; ?></strong></p>
+        <p>Balance: <?php echo explode(PHP_EOL,shell_exec("dogecoin-cli getbalance"))[0]; ?></p>
+        <p> <a href="/deposit.php" style="color: red;">Deposit</a> </p>
+        <p> <a href="/withdraw.php" style="color :red;">Withdraw</a> </p>
+        <p> <a href="/index.php" style="color: red;">Home</a> </p>
+    	<p> <a href="/index.php?logout='1'" style="color: red;">logout</a> </p>
+    <?php endif ?>
+</div>
+		
+</body>
+</html>
